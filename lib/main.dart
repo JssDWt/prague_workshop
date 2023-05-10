80dc1d08e6d8318dc09586c05a3a52281411d953
Jesse de Wit <witdejesse@hotmail.com>
2023-05-10 13:59:16 +0200

    add some UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Breez SDK Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Breez SDK Demo Home Page'),
     );
   }
 }
@@ -29,13 +30,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  int _balance = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -47,21 +42,133 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
+              '$_balance sat',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) => const ReceivePaymentDialog(),
+                      );
+                    },
+                    child: const Text("RECEIVE"),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) => const SendPaymentDialog(),
+                      );
+                    },
+                    child: const Text("SEND"),
+                  ),
+                )
+              ],
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+}
+
+class ReceivePaymentDialog extends StatefulWidget {
+  const ReceivePaymentDialog({super.key});
+
+  @override
+  State<ReceivePaymentDialog> createState() => _ReceivePaymentDialogState();
+}
+
+class _ReceivePaymentDialogState extends State<ReceivePaymentDialog> {
+  String? _invoice;
+  String? _paymentHash;
+
+  @override
+  void initState() {
+    super.initState();
+    _invoice = "random";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+        child: AspectRatio(
+          aspectRatio: 1,
+          child: SizedBox(
+            width: 230.0,
+            height: 230.0,
+            child: _invoice == null
+                ? const Center(child: CircularProgressIndicator())
+                : QrImage(data: _invoice!.toUpperCase()),
+          ),
+        ),
       ),
+    );
+  }
+}
+
+class SendPaymentDialog extends StatefulWidget {
+  const SendPaymentDialog({super.key});
+
+  @override
+  State<SendPaymentDialog> createState() => _SendPaymentDialogState();
+}
+
+class _SendPaymentDialogState extends State<SendPaymentDialog> {
+  final TextEditingController invoiceController = TextEditingController();
+  bool _payInProgress = false;
+
+  @override
+  Widget build(BuildContext context) {
+    if (_payInProgress) {
+      return Dialog(
+        backgroundColor: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              CircularProgressIndicator(),
+              SizedBox(
+                height: 15,
+              ),
+              Text('Sending payment...')
+            ],
+          ),
+        ),
+      );
+    }
+
+    return AlertDialog(
+      title: const Text("Send Payment"),
+      content: TextField(
+        decoration: const InputDecoration(label: Text("Paste invoice")),
+        controller: invoiceController,
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {},
+          child: const Text("OK"),
+        ),
+        TextButton(
+          child: const Text("CANCEL"),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        )
+      ],
     );
   }
 }
